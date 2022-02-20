cccf88ea439ca8ae4d4a55e77b9d36e29b6cb36b
Eugene <contact.duk@gmail.com>
2022-02-20 19:19:53 +0300

    Adding initial source code

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Eugene on 21.12.21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
